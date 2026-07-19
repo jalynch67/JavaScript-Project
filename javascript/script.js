de4994947e4add0047abad301f2b203364b73692
Jake Lynch <jakelynch9320@gmail.com>
2026-07-19 14:18:28 +0100

    Add live preview functionality for character name input

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,17 @@
-console.log("JavaScript is Running");
+/* -- Live Preview JS -- */
+/* -- START: Character Name change update -- */
+const nameInput = document.getElementById("character-name");
+const previewName = document.getElementById("preview-name");
+
+function updateNamePreview() {
+  const characterName = nameInput.value.trim();
+
+  if (characterName === "") {
+    previewName.textContent = "Unnamed Hero";
+  } else {
+    previewName.textContent = characterName;
+  }
+}
+
+nameInput.addEventListener("input", updateNamePreview);
+/* -- END: Character Name change update -- */
